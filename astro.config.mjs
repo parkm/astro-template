0fa7b16fe2e096160e0713c85e2646c336e08f86
Parker Miller <parkermiller00@gmail.com>
2025-04-02 20:35:36 -0600

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  })]
+  integrations: [react()],
+
+  output: 'server',
+
+  devToolbar: {
+    enabled: false,
+  },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
